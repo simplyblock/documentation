efa6f7cbf9e6f3004b0c59277103e000fc41217d
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-04-14 10:14:57 +0200

    Removed legacy paths from openapi.json specification

--- scripts/openapi-json-gen.py
+++ scripts/openapi-json-gen.py
@@ -4,5 +4,7 @@ sys.path.append('scripts/sbcli-repo')
 import json
 from simplyblock_web.app import app
 with open('docs/reference/api/openapi.json', 'w') as f:
-    json.dump(app.openapi(), f, indent=2)
+    openapi = app.openapi()
+    openapi["paths"] = {path: x for path, x in openapi["paths"].items() if path.startswith("/api/v2")}
+    json.dump(openapi, f, indent=2)
 print('Generated openapi.json')
